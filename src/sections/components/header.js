07c02c2def7524efc3d7065ec0bc08f34d578926
kathdelacruz <katherine.delacruz8@gmail.com>
2019-05-24 23:13:50 -0500

    Add header and raw list

---
+++ src/sections/components/header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    SafeAreaView,
+} from 'react-native';
+
+const Headers = (props) => {
+  return (
+    <View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Image
+            source={require('../../../assets/logo.png')}
+            style={styles.logo}
+          />
+          <View style={styles.right}>
+            {props.childen}
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 40,
+    resizeMode: 'contain',
+  },
+  container: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  }
+})
+
+export default Headers;
