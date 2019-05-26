b66bf3a18aa880ef35e6b3301063da1bc19e07a5
kathdelacruz <katherine.delacruz8@gmail.com>
2019-05-25 23:03:28 -0500

    Add 16:9 responsive video

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
-import Video from 'react-native-video';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/CategoryList';
+import Player from './src/player/containers/Player';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -26,24 +26,7 @@ export default class App extends Component<Props> {
     return (
       <Home>
         <Header />
-        <View
-          style={{
-            flex: 1,
-            height: 100,
-          }}
-        >
-          <Video
-            source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              bottom: 0,
-              top: 0,
-            }}
-            resizeMode="cover"
-          />
-        </View>
+        <Player />
         <CategoryList list={this.state.categoryList} />
         <SuggestionList list={this.state.suggestionList} />
       </Home>
