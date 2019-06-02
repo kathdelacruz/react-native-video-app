e3ffc46338641692eae0add0a39085f004d274ac
kathdelacruz <katherine.delacruz8@gmail.com>
2019-06-02 12:04:23 -0500

    Add basic animation

--- src/screens/containers/Movie.js
+++ src/screens/containers/Movie.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Animated } from 'react-native';
+
 import MovieLayout from '../components/MovieLayout';
 import Player from '../../player/containers/Player';
 import Header from '../../sections/components/Header';
@@ -7,21 +9,41 @@ import Back from '../../sections/components/Back';
 import Details from '../../videos/components/Details';
 
 class Movie extends Component {
+  state = { opacity: new Animated.Value(0) }
+
   backHome = () => {
     this.props.dispatch({
       type: 'SET_SELECTED_MOVIE',
       payload: { movie: null }
     });
   }
+
+  componentDidMount() {
+    Animated.timing(
+      this.state.opacity,
+      {
+        toValue: 1,
+        duration: 1000,
+      }
+    ).start();
+  }
+
   render() {
     return (
-      <MovieLayout>
-        <Header>
-          <Back onPress={this.backHome} />
-        </Header>
-        <Player />
-        <Details {...this.props.movie} />
-      </MovieLayout>
+      <Animated.View
+        style={{
+          flex: 1,
+          opacity: this.state.opacity,
+        }}
+      >
+        <MovieLayout>
+          <Header>
+            <Back onPress={this.backHome} />
+          </Header>
+          <Player />
+          <Details {...this.props.movie} />
+        </MovieLayout>
+      </Animated.View>
     );
   }
 }
